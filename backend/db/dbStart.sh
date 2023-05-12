0d2f78ba562652e19535b9903e0057dbb82ac773
Atkiyama <thisisatkiyama@gmail.com>
2023-05-12 14:26:33 +0900

    frontend整備

---
+++ backend/db/dbStart.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+
+mongosh --quiet <<EOF
+use shareJob
+db.dropDatabase()
+
+use shareJob
+
+
+db.companyInfo.insertOne( {
+   email: "test@test.co.jp",
+   id: 1,
+   memo: "test",
+
+} );
+
+db.company.insertOne( {
+   id: 1,
+   name:"マクロコスモス",
+   abstract:"エネルギー企業",
+   industries:["エネルギー産業"],
+   locations:["ガラル地方"],
+
+} );
+
+db.users.insertOne( {
+   email: "test@test.co.jp",
+   name: "てすとたろう",
+   password: "password",
+   companyInfoList: [1],
+
+} );
+exit
+EOF
---
+++ frontend/src/pages/top.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Top() {
+	return <div></div>;
+}
+
+export default Top;

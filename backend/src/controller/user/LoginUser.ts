d8fe396d9cc4dcc5173a4905cc453da752b2a290
Atkiyama <thisisatkiyama@gmail.com>
2023-06-20 16:55:43 +0900

    userlogin実装中

--- backend/src/controller/user/LoginUser.ts
+++ backend/src/controller/user/LoginUser.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { UserModel, User } from '../../model/user';
 import connectDB from '../../utils/database';
+import jwt from 'jsonwebtoken';
+import auth from '../../utils/auth';
 
 /**
  * ログイン時のAPI
@@ -14,6 +16,7 @@ export default async function (req: Request, res: Response) {
 
         const savedUserData: User | null = await UserModel.findOne({ email: req.body.email, password: req.body.password });
         if (savedUserData) {
+            const token = jwt.sign(savedUserData.email, process.env.SECRET_KEY!, { expiresIn: "23h" });
             return res.status(200).json({ message: 'ログイン成功', savedUserData: savedUserData });
         } else {
             return res.status(400).json({ message: 'ログイン失敗:ユーザー登録をしてください' });

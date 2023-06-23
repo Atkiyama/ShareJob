7062079415b38696203a2ced4e02bd6d13d671f2
Atkiyama <thisisatkiyama@gmail.com>
2023-06-23 21:16:36 +0900

    topページ仮完成

--- frontend/src/pages/top.tsx
+++ frontend/src/pages/top.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 /**
  * トップ画面
@@ -8,7 +8,50 @@ function Top() {
 	useEffect(() => {
 		document.title = 'ShareJob';
 	}, []);
-	return <div>ShareJobトップページ</div>;
+
+	return (
+		<div>
+			<h1>shareJobへようこそ!</h1>
+			本サイトでは主に以下のことができます
+			<ul>
+				<li>就職活動の状況の登録、管理</li>
+				{/* <li>項目2</li> */}
+			</ul>
+			<h2>就職活動の状況の登録、管理</h2>
+			<h3>登録方法</h3>
+			<p>各企業に対する就職活動の状況をメモとして登録することができます。</p>
+			<p>
+				ホーム画面の右下の+ボタンを押すと企業検索画面に移るのでそこから企業をMy企業に登録して選考状況を登録できます。
+			</p>
+			<img
+				src={require('../image/homeExample.png')}
+				alt="画像の説明"
+				style={{ width: '600px', height: '350px' }}
+			/>
+			<p>各企業に対する就職活動の状況をメモとして登録することができます。</p>
+			<p>
+				ホーム画面の右下の+ボタンを押すと企業検索画面に移るのでそこから企業をMy企業に登録して選考状況を登録できます。
+			</p>
+			<img
+				src={require('../image/homeExample.png')}
+				alt="画像の説明"
+				style={{ width: '600px', height: '350px' }}
+			/>
+			<h3>企業情報の登録</h3>
+			<p>検索に企業がヒットしない場合、自分で登録することもできます</p>
+			<p>
+				ヘッダーから登録企業を選択すると自分が今までサイトに登録した企業を閲覧できます
+			</p>
+			<p>
+				右下の+ボタンから新規登録ができるので是非企業情報の登録にご協力ください！
+			</p>
+			<img
+				src={require('../image/companyListExample.png')}
+				alt="画像の説明"
+				style={{ width: '600px', height: '350px' }}
+			/>
+		</div>
+	);
 }
 
 export default Top;
